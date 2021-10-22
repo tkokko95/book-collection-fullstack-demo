5cdd7452944ba7c309f7ae9a0fbf0a78c163115b
Taavi Kokko <taavi.kokko@gmail.com>
2021-10-22 11:21:10 +0300

    Various small cleanups

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -3,7 +3,9 @@ const info = (...params) => {
 }
 
 const error = (...params) => {
-    console.error(...params)
+    // We don't want the console to get spammed full of error messages during the unit tests.
+    // Why Jest still doesn't have a clean way of suppressing console.errors is anyone's guess
+    process.env.NODE_ENV === 'test' ? null : console.error(...params)
 }
 
 module.exports = {
