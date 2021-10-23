c6e4b6f3c775980f24c8576d7d19f130ec4950d3
Taavi Kokko <taavi.kokko@gmail.com>
2021-10-23 16:34:15 +0300

    Heroku is broken, trying whether the removal of PORT env var was the cause

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const app = require('./app')
 const http = require('http')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 const server = http.createServer(app)
 server.listen(PORT, () => {
