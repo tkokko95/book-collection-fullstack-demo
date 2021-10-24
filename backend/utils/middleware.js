20a4a64d9c40574b3a1840a0734d5f68b0c4b34c
Taavi Kokko <taavi.kokko@gmail.com>
2021-10-24 13:21:44 +0300

    Fixed incorrectly formatted ValidationError

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -12,7 +12,7 @@ const errorHandler = (error, request, response, next) => {
             return response.status(400).json({ error: error.message })
         }
     case 'ValidationError':
-        return response.status(400).json(error.message)
+        return response.status(400).json({ error: error.message })
     case 'SyntaxError':
         return response.status(400).json({ error: 'Malformatted request' })
     default:
